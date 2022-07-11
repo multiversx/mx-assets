a6be184cd9e77cef146f1b8d560d836f04e50080
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-07-11 12:26:51 +0300

    update workflow

--- .github/snippet.sh
+++ .github/snippet.sh
@@ -15,10 +15,8 @@ validate_filenames() {
 validate_file_size() {
   readarray -t files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added }}')"
   for file in ${files[@]}; do
-    file_size_kb = $(ls -s --block-size=K ${file} | grep -o -E '[0-9]+')
-    file_size_kb = $(expr ${file_size_kb})
+    file_size_kb=$(ls -s --block-size=K ${file} | grep -o -E '^[0-9]+')
 
-    echo "File size: ${file_size_kb}"
     if [[ ${file_size_kb} -gt 64 ]]; then
       echo "File ${file} is too large! (${file_size_kb} KB)"
       exit 1
