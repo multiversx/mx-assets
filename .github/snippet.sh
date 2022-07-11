8d181ec9ade9430df2eb5cff11a4fc43f93f167c
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-07-11 14:01:50 +0300

    activate file size validation

--- .github/snippet.sh
+++ .github/snippet.sh
@@ -1,9 +1,7 @@
-#!bin/bash
 DIR=""
 
 validate_filenames() {
-  jq -r '.[]' ${{ steps.files.outputs.added }} >> "added.txt"
-  readarray -t files < "added.txt"
+  readarray -t files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added }}')"
   for file in ${files[@]}; do
     if [[ ${file} != *"/info.json"* && ${file} != *"/logo.png"* && ${file} != *"/logo.svg"* ]]; then
       echo "Filename ${file} isn't expected!"
@@ -15,15 +13,14 @@ validate_filenames() {
 }
 
 validate_file_size() {
-  #readarray -t files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added }}')"
-  files=( "testnet/tokens/CTP-298075/logo.png" "testnet/tokens/CTP-298075/logo.svg" )
+  readarray -t files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added }}')"
   for file in ${files[@]}; do
     file_size_kb=$(ls -s --block-size=K ${file} | grep -o -E '^[0-9]+')
 
     echo "File size: ${file_size_kb}"
     if [[ ${file_size_kb} -gt 64 ]]; then
       echo "File ${file} is too large! (${file_size_kb} KB)"
-      #exit 1
+      exit 1
     fi
   done
 }
