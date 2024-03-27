4471d5a4ba77402196add177bd572fbb57a6333f
AlexandruPislariu <alexandru.pislariu@multiversx.com>
2024-03-27 10:05:30 +0200

    Update script

--- .github/snippet.sh
+++ .github/snippet.sh
@@ -1,8 +1,7 @@
 DIR=""
 
 validate_filenames() {
-  ADDED_FILES=$@
-  for file in $ADDED_FILES; do
+  for file in "$@"; do
     if [[ ${file} != *"/info.json"* && ${file} != *"/logo.png"* && ${file} != *"/logo.svg"* && ${file} != *"accounts/"* && ${file} != *".github/"* && ${file} != *"/ranks.json"* && ${file} != *"README.md"* ]]; then
       echo "Filename ${file} isn't expected!"
       exit 1
@@ -13,12 +12,12 @@ validate_filenames() {
 }
 
 validate_file_size() {
-  ADDED_FILES=$@
   SIZE_LIMIT=100
-  for file in $ADDED_FILES; do
+  for file in "$@"; do
     if [[ ${file} == *"/logo.svg"* || ${file} == *"/logo.svg"* ]]; then
       file_size_blocks=$(ls -sh ${file} | grep -o -E '^[0-9]+')
       file_size_kb=$(expr ${file_size_blocks} / 2)
+      echo "File size: ${file_size_blocks} KB"
 
       if [[ ${file_size_kb} -gt $SIZE_LIMIT ]]; then
         echo "File ${file} is too large! (${file_size_kb} KB)"
@@ -29,9 +28,7 @@ validate_file_size() {
 }
 
 validate_svg_square() {
-  ADDED_FILES=$@
-
-  for file in $ADDED_FILES; do
+  for file in "$@"; do
     if [[ ${file} == *"/logo.svg"* ]]; then
       view_box=$(cat $file | grep -E " viewBox" | grep -E -o "(([0-9]*\.[0-9]+|[0-9]+) ){3,3}([0-9]*\.[0-9]+|[0-9]+)" | head -1)
 
@@ -56,10 +53,8 @@ validate_svg_square() {
 }
 
 validate_png_dimensions() {
-  ADDED_FILES=$@
   EXPECTED_PNG_DIMENSIONS="200 x 200"
-
-  for file in $ADDED_FILES; do
+  for file in "$@"; do
     if [[ ${file} == *"/logo.png"* && (${file} == *"identities/"* || ${file} == *"tokens/"*) ]]; then
       png_dimensions=$(file $file | grep -E -o "[0-9]+ x [0-9]+" | head -1)
 
@@ -76,7 +71,7 @@ validate_png_dimensions() {
 }
 
 validate_token_existence() {
-  urls=( "https://api.elrond.com/" "https://testnet-api.elrond.com/" "https://devnet-api.elrond.com/")
+  urls=( "https://api.multiversx.com/" "https://testnet-api.elrond.com/" "https://devnet-api.elrond.com/")
   for url in ${urls[@]}; do
     API_TOKEN_URL="$url" 
     API_TOKEN_URL+="tokens/"
